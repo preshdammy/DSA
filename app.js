fa15c1930f921f30475117d840364aba4dbc16bc
Precious Akinwumi <preciousdammy02@gmail.com>
2025-11-17 15:00:59 +0100

    ..

--- app.js
+++ app.js
@@ -3,26 +3,9 @@ const port = 5500 | process.env.PORT
 const app = express()
 
 app.get("/" , (req, res) =>{
-    res.status(200).json([
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-        {obj:"1"},
-    ])
+   res.send(
+    "Welcome to Express Server. Preciou here"
+   )
 })
 
 app.listen(port ,()=> `${port} Connected`)
